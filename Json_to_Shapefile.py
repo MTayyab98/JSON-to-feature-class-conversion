96f562af6f40bec57b4cc60c42bb03bd1a05889b
MTayyab98 <52675331+MTayyab98@users.noreply.github.com>
2021-06-13 13:35:44 +0530

    Update Json_to_Shapefile.py

--- Json_to_Shapefile.py
+++ Json_to_Shapefile.py
@@ -1,10 +1,10 @@
 import arcpy
 import os
-os.chdir("D:\\tayyab\\New folder\\Canal\\data\\data")
-arcpy.env.workspace = "D:\\tayyab\\New folder\\Canal\\data\\data"
-arcpy.env.overwriteOutput = True
+#os.chdir("D:\\tayyab\\New folder\\Canal\\data\\data")
+#arcpy.env.workspace = "D:\\tayyab\\New folder\\Canal\\data\\data"    #Initialising workspace
+arcpy.env.overwriteOutput = True     #For overwriting output if running code multiple times
 arr = os.listdir('.')   #returns a list containing the names of the entries in the directory
 
-for i in arr:   
+for i in arr:          #Gives the output in the same directory
     arcpy.JSONToFeatures_conversion(i, ("output3{}.shp").format(i)) #converts JSON to Feature class
     
